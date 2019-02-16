47bdcbc4f56d59837ffda9b01cca1c7365832dd4
Syed Ajaz Haider Zaidi <syedajaz.zaidi@gmail.com>
2019-02-16 10:50:51 +0530

    Add files via upload

---
+++ jdbc/src/jdbc/Jdbc.java
@@ -0,0 +1,35 @@
+package jdbc;
+
+import java.sql.*; 
+class Jdbcexample 
+{
+    public static void main(String a[]) throws Exception
+    { 
+    		Class.forName("oracle.jdbc.OracleDriver");
+        
+        String url = "jdbc:oracle:thin:@localhost:1521:xe"; 
+        String user = "system"; 
+        String pass = "Capgemini123"; 
+  
+            
+             Connection con = DriverManager.getConnection(url,user,pass); 
+  
+         try {   
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("select * from studentdetails");
+            
+           while(rs.next()) {
+            	
+            	System.out.println(rs.getInt(1) + " " + rs.getString(3));
+            	
+            }
+         }
+           catch(Exception ex)
+           {
+        	   System.out.println("ex");
+           }
+            con.close(); 
+   
+       
+    } 
+} 
